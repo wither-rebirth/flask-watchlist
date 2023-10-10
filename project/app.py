6adae40fead9525e026e6e2672bed78ad8982611
wither-rebirth <xiaoqian1942@outlook.com>
2023-10-10 15:53:29 +0800

    User authentication with Flask-Login

--- project/app.py
+++ project/app.py
@@ -6,6 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 import click
 from markupsafe import escape
 from flask import url_for,request,redirect,flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager
+from flask_login import login_user, login_required,logout_user, current_user,UserMixin
 
 WIN = sys.platform.startswith('win')
 if WIN: #如果是Windows系统,使用三个斜线:
@@ -13,7 +16,9 @@ if WIN: #如果是Windows系统,使用三个斜线:
 else: #否则四条斜线:
     prefix = 'sqlite:////'
     
-app = Flask(__name__)    
+app = Flask(__name__)   
+login_manager = LoginManager(app) #实例化扩展类 
+login_manager.login_view = 'login'
 app.config['SECRET_KEY'] = 'dev'
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭对模型修改的监控
@@ -32,6 +37,8 @@ def page_not_found(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated: #如果当前用户未认证
+            return redirect(url_for('index')) #重定向到主页
         #获取表单数据
         title = request.form.get('title') #传入表单对应输入字段的name值
         year = request.form.get('year')
@@ -49,7 +56,37 @@ def index():
     movies = Movie.query.all() #读取所有电影记录
     return render_template('index.html', movies=movies)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if not username or not password:
+            flash('Invalid input')
+            return redirect(url_for('login'))
+        
+        user= User.query.first()
+        #验证用户名和密码是否一致
+        if username == user.username and user.validate_password(password):
+            login_user(user) #登入用户
+            flash('Login success')
+            return redirect(url_for('index')) #重定向到主页
+        
+        flash('Invalid username or password.') #如果验证失败，显示错误信息
+        return redirect(url_for('login')) #重定向回登录页面
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required #用于视图保护，后面会详细介绍
+def logout():
+    logout_user() #登出用户
+    flash('Goodbye')
+    return redirect(url_for('index')) #重定向回首页
+
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required #登陆保护
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     
@@ -70,6 +107,7 @@ def edit(movie_id):
     return render_template('edit.html', movie = movie) #传入被编辑的电影记录
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST']) #限定只接受POST请求
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id) #获取电影记录
     db.session.delete(movie) #删除对应的记录
@@ -93,6 +131,26 @@ def test_url_for():
     print(url_for('test_url_for', num=2)) #输出：/test?num=2
     return 'Test page'
 
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+        
+        if not name or len(name) > 20:
+            flash('Invalid input')
+            return redirect(url_for('settings'))
+        
+        current_user.name = name
+        #current_user 会返回当前登录用户的数据库记录对象
+        #等同于下面的用法
+        #user = User.query.first()
+        #user.name = name
+        db.session.commit()
+        flash('Settings update')
+        return redirect(url_for('index'))
+    return render_template('settings.html')
+
 @app.cli.command()
 def forge():
     """Generate fake data"""
@@ -101,6 +159,7 @@ def forge():
     # 全局的两个变量移动到这个函数内
     name = 'George Chen'
     movies = [
+        {'title':'HarryPotter', 'year':'2002'},
         {'title':'My Neighbor Totoro', 'year': '1988'},
         {'title':'Dead Poets Society', 'year': '1989'},
         {'title': 'A Perfect World', 'year': '1993'},
@@ -119,15 +178,25 @@ def forge():
         db.session.add(movie)
     db.session.commit()
     click.echo('Done')
+    
+class User(db.Model, UserMixin):
+    id= db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(20))
+    username=db.Column(db.String(20)) #用户名
+    password_hash = db.Column(db.String(128)) #密码散列值
+    
+    def set_password(self, password): #用来设置密码的方法，接受密码作为参数
+        self.password_hash = generate_password_hash(password) #将生成的密码放置到对应字段
+    
+    def validate_password(self, password): #用于验证密码的方法，接受密码作为参数
+        return check_password_hash(self.password_hash, password) #返回布尔值
 
-class User(db.Model): #表名将会是user (自动生成，小写处理)
-    id = db.Column(db.Integer, primary_key=True) #主键
-    name = db.Column(db.String(20)) #名字
     
 class Movie(db.Model): #表名将会是 movie
     id = db.Column(db.Integer, primary_key=True) #主题
     title = db.Column(db.String(60)) #电影标题
     year = db.Column(db.String(4)) #电影年份
+
 
 @app.cli.command() #注册为命令，可以传入name 参数来自定义命令
 @click.option('--drop', is_flag=True, help='Create after drop.')#设置选项
@@ -137,3 +206,29 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database.') #输出提示消息
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.' )
+def admin(username, password):
+    """Create user."""
+    db.create_all()
+    
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password) #设置密码
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password) #设置密码
+        db.session.add(user)
+    
+    db.session.commit() #提交数据库会话
+    click.echo('Done')
+
+@login_manager .user_loader
+def load_user(user_id): #创建用户加载回调函数，接受用户ID作为参数
+    user = User.query.get(int(user_id)) #用ID作为User模型的主键查询对应的用户
+    return user #返回用户对象
